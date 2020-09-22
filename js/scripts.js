ef904f262f74f470dd1e86b6e19807e828591bf6
Devon Proudfoot <proudfoot.devon@gmail.com>
2020-09-22 00:18:59 +0000

    Added challenge switching functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(){
-  
+document.addEventListener('DOMContentLoaded', function() {
+  const navBar = document.getElementById('navbar');
+
+  function getId(element) {
+    return element.id.slice(-1);
+  }
+
+  function changeChallenge(identifier) {
+    document.getElementsByClassName('active')[0].classList.remove('active')
+    document.getElementById(`challenge${identifier}`).classList.add('active');
+  }
+
+  navBar.addEventListener('click', function(e) {
+    e.preventDefault();
+    const challengeId = getId(e.target)
+    changeChallenge(challengeId);
+  });
 });
