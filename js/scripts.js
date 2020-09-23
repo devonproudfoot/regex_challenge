7c91f88a1186f066e4537ac2e40cf8e73ac25631
Devon Proudfoot <proudfoot.devon@gmail.com>
2020-09-23 01:19:08 +0000

    Started adding challenge js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const navBar = document.getElementById('navbar');
+  const challenges = document.getElementById('content');
 
   function getId(element) {
     return element.id.slice(-1);
@@ -10,9 +11,40 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById(`challenge${identifier}`).classList.add('active');
   }
 
+  function checkAnswerValidity() {
+    return 0;
+  }
+
+  function updateResultMessage(result, resultHeader) {
+    let message;
+    message = result ? 'Great Job!' : 'Try Again!';
+    resultHeader.innerText = message;
+  }
+
+  // Add click events for the validation buttons
+  challenges.addEventListener('click', function(e) {
+    if (e.target.classList.contains('validate')) {
+      e.preventDefault();
+      const resultMessage = e.target.nextElementSibling;
+      const parentDiv = e.target.parentElement.parentElement;
+      const input = parentDiv.children[1];
+      const values = parentDiv.children[2];
+
+      console.log(input, values);
+
+      //const validAnswer = checkAnswerValidity();
+      const validAnswer = false;
+      updateResultMessage(validAnswer, resultMessage);
+
+    }
+  });
+
+  // Add Click event for navbar to change challenges
   navBar.addEventListener('click', function(e) {
-    e.preventDefault();
-    const challengeId = getId(e.target)
-    changeChallenge(challengeId);
+    if (e.target.classList.contains('NavBar-Selector')) {
+      e.preventDefault();
+      const challengeId = getId(e.target)
+      changeChallenge(challengeId);
+    }
   });
 });
